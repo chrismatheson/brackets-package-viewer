8f4d10026dfd2f7bf8bb26955f4baf19fb1807f4
Chris Matheson <chris.matheson@ivendi.com>
2014-02-09 14:58:52 +0000

    adding things

--- main.js
+++ main.js
@@ -1,9 +1,6 @@
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
 /*global define, $, brackets, Mustache */
 
-/** {ext_name} Extension
-    description
-*/
 define(function (require, exports, module) {
     'use strict';
 
--- packageViewer.js
+++ packageViewer.js
@@ -4,26 +4,41 @@ define(function (require, exports, module) {
     "use strict";
 
     var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
+    var FileIndexManager = brackets.getModule("project/FileIndexManager");
 
-    var template, pkg;
+    var template, pkgmodel = [], $viewContainer;
 
     ExtensionUtils.loadFile(module, './panel.tpl.html').complete(function (data) {
         template = data.responseText;
     });
     ExtensionUtils.loadStyleSheet(module, './panel.less');
 
-    ExtensionUtils.loadFile(module, './package.json').complete(function (data) {
-        pkg = JSON.parse(data.responseText);
-    });
+    //create the container
+    $viewContainer = $('<div class="package-viewer"></div');
 
-    function getCustomViewHolder() {
-        return $(Mustache.render(template, pkg));
+
+    function getCustomViewHolder(fullPath) {
+        //clear out children from last render;
+        $viewContainer.children().remove();
+        //return an object and later fill it in
+        return $viewContainer;
     }
 
     /**
      * traverse up project hierachy, starting at fullPath, building our view model
      */
     function render(fullPath) {
+        FileIndexManager.getFilenameMatches('all', 'package.json').then(function (packages) {
+            packages.forEach(function (file) {
+                //load package.json
+                ExtensionUtils.loadFile(module, './package.json').complete(function (data) {
+                    pkgmodel.push(JSON.parse(data.responseText));
+                    $(Mustache.render(template, pkgmodel)).appendTo($viewContainer);
+                });
+                var readmePath = file._parentPath + 'README.md';
+            });
+        });
+
         // use github for rendering markdown - http://developer.github.com/v3/markdown/
         console.log('render called');
     }
